53f44ce58b06afb416ff4e3189364247aacc47a2
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-05-30 12:20:09 +0000

    feat(ip): adicionar função assíncrona para obter endereço IP

---
+++ funcs/ ip.js
@@ -0,0 +1,13 @@
+async function getIP() {
+    try {
+        const response = await fetch('https://api.ipify.org?format=json');
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data.ip;
+    } catch (error) {
+        console.error('Error fetching IP address:', error);
+        return null;
+    }
+}
