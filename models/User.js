ab65f423b248fc1699cc67cd96158df9f3d173ee
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-06-13 10:57:36 +0000

    feat(user): adicionar campo de imagem com valor padrão

--- models/User.js
+++ models/User.js
@@ -32,6 +32,11 @@ const User = db.define("User", {
     address: {
         type: DataTypes.STRING,
         allowNull: true
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     }
 })
 
