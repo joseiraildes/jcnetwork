0acfffcdf30f5ae1c00369178128e09450e61c69
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-06-15 22:11:58 +0000

    feat(upload): add multer configuration for file uploads

---
+++ upload/upload.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const { diskStorage } = require("multer");
+
+const storage = diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images/");
+    },
+
+    filename: (req, file, cb) => {
+        const fileExtension = file.originalname.split(".")[1]
+
+        const newFileName = require('crypto')
+            .randomBytes(64)
+            .toString('hex');
+
+        cb(null, `${newFileName}.${fileExtension}`);
+    }
+})
+
+const upload = multer({ storage })
+
+module.exports = upload
